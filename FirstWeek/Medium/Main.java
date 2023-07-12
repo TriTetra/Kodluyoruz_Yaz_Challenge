1728adb049f3d51df6da15f4c30d29afc175ab9d
Abdullokh Abdukodirov <55631365+TriTetra@users.noreply.github.com>
2023-07-13 00:19:28 +0300

    Delete FirstWeek/Medium directory

--- FirstWeek/Medium/Main.java
+++
@@ -1,8 +0,0 @@
-package FirstWeek.Medium;
-
-public class Main {
-    public static void main(String[] args) {
-        Process p = new Process();
-        System.out.println(p.checkValue());
-    }
-}
